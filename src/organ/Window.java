773c533c622c52baa46ababdfb290726695aff15
cabobalot <casey8cockrell@gmail.com>
2020-01-03 15:13:56 -0700

    Finally formating looks good,
    made the stop grids fill vertically so stops in same harmonic base are in nice lists. Note tht a stoplist with not multipules of 8 stops to a set will break formating

--- src/organ/Window.java
+++ src/organ/Window.java
@@ -20,12 +20,12 @@ public class Window extends JPanel implements ItemListener {
     public Window() {
     	super(new GridLayout(0, 1));
         
-        readStopsFile2();
+        readStopsFile();
         
         setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
     }
     
-    private void readStopsFile3() {
+    private void readStopsFile() {
     	
     	ArrayList<JPanel> divisions = new ArrayList<JPanel>();
     	stops = new ArrayList<JCheckBox>();
@@ -75,103 +75,10 @@ public class Window extends JPanel implements ItemListener {
 	        stops.add(nextStop); // add check to list
 	        
 	        GridBagConstraints gb = new GridBagConstraints();
-	        gb.gridy = GridBagConstraints.RELATIVE;
-	        gb.gridx = GridBagConstraints.RELATIVE;
+	        gb.gridy = 0;
+	        gb.gridx = 0;
+	        gb.anchor = GridBagConstraints.WEST;
 	        stopGrid.add(nextStop, gb); // add check to grid
-	    	
-	    	stopName = sc.nextLine(); // read next stop
-	    	stopFeet = sc.nextLine();
-	    	myDivision = sc.nextLine();
-	    	trash = sc.nextLine();
-	    	while (!trash.equals("=")) { // throw away unused info
-	    		trash = sc.nextLine();
-	    	}
-	    	while (curDivision.equals(myDivision)) { // loop through division
-
-		    	JCheckBox nextNextStop = new JCheckBox(stopName + " : " + stopFeet); // create checkbox
-		    	System.out.println(myDivision + " Stop:" + stopName + " " + stopFeet);
-		    	nextStop.setSelected(false);
-		        nextStop.addItemListener(this);
-		        
-		        stops.add(nextNextStop); // add check to grid
-		        
-//		        gb.gridy = gb.gridy + 1;
-//		        gb.gridx++;
-		        stopGrid.add(nextStop, gb); // add check to grid
-		    	
-		        if (sc.hasNext()) {
-			    	
-		    	}
-		    	if (sc.hasNext()) {
-		    		stopName = sc.nextLine(); //read next stop
-			    	stopFeet = sc.nextLine();
-			    	myDivision = sc.nextLine();
-			    	trash = sc.nextLine();
-			    	while (!trash.equals("=")) { // throw away unused info
-			    		trash = sc.nextLine();
-			    	}
-		    	}
-		    	else {
-		    		break;
-		    	}
-		    	
-	    	}
-	    	
-	    	add(divisionPanel);
-	    }
-	    
-	    
-	    sc.close();
-    }
-    
-    private void readStopsFile2() {
-    	ArrayList<JPanel> divisions = new ArrayList<JPanel>();
-    	stops = new ArrayList<JCheckBox>();
-    	
-    	File file = new File("./Stops.txt");
-    	Scanner sc;
-	    try {
-			sc = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
-	    
-	    String curDivision = "";
-	    String stopName = "";
-	    String stopFeet = "";
-	    
-	    
-	    stopName = sc.nextLine(); // read from file
-    	stopFeet = sc.nextLine();
-    	String myDivision = sc.nextLine();
-    	String trash = sc.nextLine();
-    	while (!trash.equals("=")) { // throw away unused info
-    		trash = sc.nextLine();
-    	}
-    	
-    	
-	    while (sc.hasNext()) {
-	    	
-	    	curDivision = myDivision;
-
-	    	JPanel stopGrid = new JPanel(new GridLayout(5, 5)); // create new grid
-	    	
-	    	JPanel divisionPanel = new JPanel(new BorderLayout()); // create new division
-	    	divisionPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK)); // border
-	    	divisionPanel.add(new JLabel(myDivision), BorderLayout.NORTH); // add label to division
-	    	divisionPanel.add(stopGrid, BorderLayout.CENTER); // add grid to division
-	    	
-	    	divisions.add(divisionPanel); // add division to list
-	    	
-	    	JCheckBox nextStop = new JCheckBox(stopName + " : " + stopFeet); //create checkbox
-	    	System.out.println(myDivision + " Stop:" + stopName + " " + stopFeet);
-	    	nextStop.setSelected(false);
-	        nextStop.addItemListener(this);
-	        
-	        stops.add(nextStop); // add check to list
-	        stopGrid.add(nextStop); // add check to grid
 	    	
 	    	stopName = sc.nextLine(); // read next stop
 	    	stopFeet = sc.nextLine();
@@ -188,11 +95,15 @@ public class Window extends JPanel implements ItemListener {
 		    	nextNextStop.addItemListener(this);
 		        
 		        stops.add(nextNextStop); // add check to grid
-		        stopGrid.add(nextNextStop);
+		        
+		        gb.gridy++;
+		        if ((gb.gridy % 8) == 0) {
+		        	gb.gridx++;
+		        	gb.gridy = 0;
+		        }
+		        
+		        stopGrid.add(nextNextStop, gb); // add check to grid
 		    	
-		        if (sc.hasNext()) {
-			    	
-		    	}
 		    	if (sc.hasNext()) {
 		    		stopName = sc.nextLine(); //read next stop
 			    	stopFeet = sc.nextLine();
@@ -213,69 +124,8 @@ public class Window extends JPanel implements ItemListener {
 	    
 	    
 	    sc.close();
-	    
     }
     
-    private void readStopsFile() {
-    	
-    	//Put the check boxes in a grid in a panel
-    	ArrayList<JPanel> panels = new ArrayList<JPanel>();
-    	ArrayList<JPanel> divisions = new ArrayList<JPanel>();
-    	
-    	stops = new ArrayList<JCheckBox>();
-    	
-    	File file = new File("./Stops.txt");
-    	Scanner sc;
-	    try {
-			sc = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
-	    
-	    String curDivision = "";
-	    String stopName = "";
-	    String stopFeet = "";
-	    
-	    while (sc.hasNext()) {
-	    	stopName = sc.nextLine();
-	    	stopFeet = sc.nextLine();
-	    	String myDivision = sc.nextLine();
-	    	
-	    	if (!curDivision.equals(myDivision)) {
-	    		
-	    		panels.add(new JPanel(new GridLayout(10, 10)));
-	    		BorderLayout divisionLayout = new BorderLayout();
-	    		divisionLayout.addLayoutComponent(new JLabel(myDivision), BorderLayout.NORTH);
-	    		divisionLayout.addLayoutComponent(panels.get(panels.size() - 1), BorderLayout.CENTER);
-	    		divisions.add(new JPanel(divisionLayout));
-
-	    		panels.get(panels.size() - 1).setBorder(BorderFactory.createLineBorder(Color.BLACK));
-	    		
-	    		add(divisions.get(divisions.size() - 1));
-	    		
-	    		curDivision = myDivision;
-	    	}
-	    	
-	    	JCheckBox nextStop = new JCheckBox(stopName + " : " + stopFeet);
-	    	System.out.println(myDivision + " Stop:" + stopName + " " + stopFeet);
-	    	
-	    	String trash = sc.nextLine();
-	    	while (!trash.equals("=")) {
-	    		trash = sc.nextLine();
-	    	}
-	    	
-	        nextStop.setSelected(false);
-	        nextStop.addItemListener(this);
-	        
-	        stops.add(nextStop);
-	        panels.get(panels.size() - 1).add(nextStop);
-	    }
-	    add(panels.get(panels.size() - 1));
-	    sc.close();
-    }
- 
     /** Listens to the check boxes. */
     public void itemStateChanged(ItemEvent e) {
         
